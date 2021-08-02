bada1aea191e5bad4ad39193bb1ff2dd0cdf24ef
Yann Klein <yann.klein@me.com>
2021-08-02 19:40:46 +0900

    start of lecture

--- src/index.js
+++ src/index.js
@@ -1,98 +1,68 @@
-// console.log("Hi Batch #595");
+console.log("Hi Batch #656");
 
 // ////////
 // Select DOM elements
 // ////////
 
 // with its ID (Sae)
-const allanImg = document.querySelector("#allan");
-// console.log(allanImg);
+
 
 // with its tag type (h2)
-const h2 = document.querySelector("h2");
-// console.log(h2);
+
 
 // with its class (group)
-const studentGroups = document.querySelector(".group");
-// console.log(studentGroups);
+
 
 // by nest selection (ul of the group)
-const ul = studentGroups.querySelector("ul");
-// console.log(ul);
+
+
 // select multiple elements (all the card)
-const cards = document.querySelectorAll(".card");
-// console.log(cards);
+
+
 // ////////
 // Modify the DOM
 // ////////
 
 // add an HTML element (SafeSpaces FC 🗼)
 // console.log(ul);
-ul.insertAdjacentHTML(
-  "beforeend", 
-  `<li>Kyoro <em>FC</em> 🗼</li>`);
+
 
 // same but more complex (add Pierre!!)
-const billsTeam = document.querySelector("#bills ul");
-billsTeam.insertAdjacentHTML(
-  "afterbegin",
-  `<li>
-    <img src="https://avatars.githubusercontent.com/u/44258144?v=4" alt="">
-    <p>Simon</p>
-  </li>`);
+
 
 // change the element style (group background to green)
 // Careful! background-color => backgroundColor
-studentGroups.style.backgroundColor = "green";
+
 
 // add a class to the element (add text-white to group)
 // Careful! no DOT for classList
-studentGroups.classList.add("text-white");
+
 
 // remove it
-studentGroups.classList.remove("text-white");
+
 
 // add/remove every 1sec
-setInterval(() => {
-  studentGroups.classList.toggle("text-white");
-}, 4000);
+
 
 // Read/Write for inputs (change the value in the email input)
-const emailInput = document.querySelector("#email");
-// console.log(emailInput.value);
-emailInput.value = "yann@lewagon.org";
+
 
 // Read/Write the (inner) text and HTML content (FF RenRaku ⚽️ title)
-const h3Baiku = document.querySelector("#ff-baiku > h3");
-console.log(h3Baiku.innerText);
-console.log(h3Baiku.innerHTML);
 
-h3Baiku.innerHTML = "Hello <strong>#595</strong>";
 
 // ////////
 // Add event listeners
 // ////////
 
-// // 1. Select SAe's picture
-// const allan = document.querySelector("#allan");
-// // 2. Listen to a click
-// allan.addEventListener("click", (event) => {
-//   // 3. Modify the DOM (add the class selected)
-//   // event.currentTarget referes to what you click
-//   event.currentTarget.classList.toggle("selected");
-// });
+// // 1. Select Sae's picture
+
+// 2. Listen to a click
+
+// 3. Modify the DOM (add the class selected)
 
 
 // 1. Select all pictures
-const pictures = document.querySelectorAll(".card img");
 
-console.log(pictures);
+// 2. Listen to a click
 
-pictures.forEach((picture) => {
-  // 2. Listen to a click
-  picture.addEventListener("click", (event) => {
-    // 3. Modify the DOM
-    event.currentTarget.classList.toggle("selected");
-  });
-});
-
+// 3. Modify the DOM
